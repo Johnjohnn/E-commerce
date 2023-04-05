10b286593579a292861a9b55ef7b1d8930b6826e
johnjohnn <conde2010@aol.com>
2023-04-05 17:56:48 -0400

    fixing the category component

--- src/components/Category-menu/category-menu.component.jsx
+++ src/components/Category-menu/category-menu.component.jsx
@@ -1,15 +1,16 @@
 import CategoryItem from "../category-item/category-item.component"
-import ".category-menu.style.scss"
-import '../';
+import "./category-menu.style.scss"
+import "../category-item/category-item.styles.scss"
 
 const MenuItems = ({categories}) =>{
+    return(
     <div className="categories-container">
       {categories.map((category) => ( 
         <CategoryItem key={category.id} category={category}/>
     ))}
 
   </div>
-    
+    ) 
 }
 
 export default MenuItems;
