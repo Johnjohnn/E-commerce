a4c10448a2e56d1e9f5cd08d5524ee7064d6955d
johnjohnn <conde2010@aol.com>
2023-04-12 18:43:32 -0400

    adding firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import { initializeApp} from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAfr41g2wX4Abux3EPR9qstMv_RveGkCao",
+    authDomain: "crown-store-179f0.firebaseapp.com",
+    projectId: "crown-store-179f0",
+    storageBucket: "crown-store-179f0.appspot.com",
+    messagingSenderId: "1090778437593",
+    appId: "1:1090778437593:web:09b9bf2e7c518f72a26bc8"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
