870abc460a3056f18d6f9bfbe7089676afd29f82
johnjohnn <conde2010@aol.com>
2023-05-15 12:53:17 -0400

    workin on the sign in feature

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp} from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from "firebase/auth"
-import {getFirestone, doc, getDoc, setDoc} from "firebase/firestone";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider,} from "firebase/auth"
+import {getFirestore, doc, getDoc, setDoc,} from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAfr41g2wX4Abux3EPR9qstMv_RveGkCao",
@@ -24,3 +24,35 @@ const firebaseConfig = {
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
   export const db = getFirestore();
+ 
+  export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
+    
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists())
+    if(!userSnapshot.exists()) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+      
+      try {
+        await setDoc (userDocRef, {
+          displayName,
+          email, 
+          createdAt
+        })
+      } catch(error) {
+        console.log("error creating the user")
+      }
+    }
+    return userDocRef;
+    // if data doesn't exist 
+
+    // check if data exists 
+
+    // return userDocRef
+  }
+
+
